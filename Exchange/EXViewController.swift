362614b6027a57254b5b88d48652e7f5b548601a
Flávio Arêas <vflavio9@gmail.com>
2024-12-30 16:28:26 -0300

    chore: initial app config

---
+++ Exchange/EXViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EXViewController.swift
+//  Exchange
+//
+//  Created by Flávio Arêas on 30/12/24.
+//
+
+import UIKit
+
+class EXViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPurple
+    }
+}
---
+++ Exchange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exchange
+//
+//  Created by Flávio Arêas on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
