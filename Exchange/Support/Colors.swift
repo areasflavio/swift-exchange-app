d250499be48430e1f04d2a817baff98216bec1ee
Flávio Arêas <vflavio9@gmail.com>
2024-12-30 17:00:24 -0300

    chore: add assets and styles

---
+++ Exchange/Support/Colors.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  Exchange
+//
+//  Created by Flávio Arêas on 30/12/24.
+//
+
+import UIKit
+
+public struct Colors {
+    
+    // Purple Shades
+    static let purpleBase = UIColor(red: 123/255, green: 58/255, blue: 237/255, alpha: 1)       // #7C3AED
+    static let purpleLight = UIColor(red: 237/255, green: 232/255, blue: 254/255, alpha: 1)     // #EDE8FE
+    
+    // Gray Shades (originals)
+    static let gray100 = UIColor(red: 15/255, green: 22/255, blue: 42/255, alpha: 1)            // #OF162A
+    static let gray200 = UIColor(red: 51/255, green: 65/255, blue: 86/255, alpha: 1)            // #334156
+    static let gray300 = UIColor(red: 148/255, green: 163/255, blue: 174/255, alpha: 1)         // #94A3B8
+    static let gray400 = UIColor(red: 248/255, green: 250/255, blue: 252/255, alpha: 1)         // #F8FAFC
+    
+    // Base
+    static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)           // #FFFFFF
+}
---
+++ Exchange/Support/Typography.swift
@@ -0,0 +1,18 @@
+//
+//  Typography.swift
+//  Exchange
+//
+//  Created by Flávio Arêas on 30/12/24.
+//
+
+import UIKit
+
+public struct Typography {
+    
+    // Title Fonts
+    static let titleXL = UIFont(name: "Inter-SemiBold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .semibold)
+    
+    // Text Fonts
+    static let textMD = UIFont(name: "Inter-Regular", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let textSM = UIFont(name: "Inter-Regular", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .regular)
+}
